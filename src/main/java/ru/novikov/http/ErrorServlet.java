eaeb4e7e4437b607c58db4286da742feaf22e279
Andrey Novikov <dromgnh@gmail.com>
2016-12-27 18:48:03 +0300

    redirecting and showing errors to user

---
+++ src/main/java/ru/novikov/http/ErrorServlet.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Андрей on 27.12.2016.
+ */
+@javax.servlet.annotation.WebServlet(name = "ErrorServlet")
+public class ErrorServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
